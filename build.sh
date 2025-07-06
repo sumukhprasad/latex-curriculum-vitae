ca7bc3053e6d6e1dca0fd6d3c9426e45b6d6d49e
Sumukh Prasad <sumukhprasad@icloud.com>
2025-07-06 11:01:36 +0530

    formatting changes and code cleanup

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 echo "\033[1mStarting pdfLaTeX...\033[0m"
-pdflatex --file-line-error --synctex=1 "example.tex"
+xelatex --file-line-error --synctex=1 "example.tex"
 
 echo
 echo "\033[1mRemoving AUX files...\033[0m"
 rm example.aux
 rm example.log
 rm example.synctex.gz
-# open ./example.pdf
+open ./example.pdf
 
 
 echo
