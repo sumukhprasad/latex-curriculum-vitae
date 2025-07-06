78a1038cdde59690151cf3ef3067b9b37e20511f
Sumukh Prasad <sumukhprasad@icloud.com>
2025-07-06 09:37:02 +0530

    cleanup

--- build-ctan-dist.sh
+++ build-ctan-dist.sh
@@ -1,5 +1,3 @@
-
-
 echo "\033[1mCreating directory...\033[0m"
 mkdir ../ctan-dist
 
@@ -9,8 +7,8 @@ rsync -av --exclude=".*" ./* "../ctan-dist/"
 mv ../ctan-dist ./ctan-dist
 cd ctan-dist
 
-mv main.tex curriculum-vitae-example.tex
-mv main.pdf curriculum-vitae-example.pdf
+mv example.tex curriculum-vitae-example.tex
+mv example.pdf curriculum-vitae-example.pdf
 rm -rf .git
 rm -rf __MACOSX
 rm build.sh
@@ -21,3 +19,4 @@ cd ..
 mv ctan-dist curriculum-vitae
 zip -vr curriculum-vitae.zip curriculum-vitae/ -x "*.DS_Store"
 mv curriculum-vitae ctan-dist
+rm example.pdf
